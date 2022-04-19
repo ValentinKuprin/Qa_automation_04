a573e85551aeec1033317260127702406271e186
Valentin <cricket666@yandex.ru>
2022-04-19 17:51:53 +0300

    add new strinng hello

--- src/JavaForBeginner/homework/Hw8/HW8.java
+++ src/JavaForBeginner/homework/Hw8/HW8.java
@@ -3,6 +3,7 @@ package JavaForBeginner.homework.Hw8;
 public class HW8 {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello");
 
     }
 }
