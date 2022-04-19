32e482871160a49935f73c0371a110f40d72b933
Valentin <cricket666@yandex.ru>
2022-04-19 18:03:45 +0300

    new hw8_1

--- src/JavaForBeginner/homework/Hw8/HW8_1.java
+++ src/JavaForBeginner/homework/Hw8/HW8_1.java
@@ -1,9 +1,15 @@
 package JavaForBeginner.homework.Hw8;
 
 public class HW8_1 {
-    Integer a = 5;
-    Double b = 5.5;
-    String s = "Hello";
+
+    public static void main(String[] args) {
+        Integer a = 5;
+        Double b = 5.5;
+        String s = "Hello";
+        String s1 = "Hello";
+
+    }
+
 
 
 }
