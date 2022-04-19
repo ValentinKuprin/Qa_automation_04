445e7ba62499bfa12dce35cc41719d0e37611df1
xbrookx <cricket666@yandex.ru_job>
2022-04-19 08:04:52 +0300

    add hw8 class

---
+++ src/JavaForBeginner/homework/Hw8/HW8.java
@@ -0,0 +1,9 @@
+package JavaForBeginner.homework.Hw8;
+
+public class HW8 {
+
+    Integer a = 5;
+    Double b = 5.5;
+    String s = "Hello";
+
+}
---
+++ src/JavaForBeginner/homework/Hw8/HW8_1.java
@@ -0,0 +1,9 @@
+package JavaForBeginner.homework.Hw8;
+
+public class HW8_1 {
+    Integer a = 5;
+    Double b = 5.5;
+    String s = "Hello";
+
+
+}
