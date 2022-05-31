b94f7f2e08ec105ada02981589b3ce12b5deba8d
xbrookx <cricket666@yandex.ru>
2022-05-31 15:29:57 +0300

    Sergey hw13 task 3

--- src/Sergey/HW/Hw13.java
+++ src/Sergey/HW/Hw13.java
@@ -18,7 +18,6 @@ public class Hw13 {
         list.add("Blue");
         list.add("Blu");
 
-
         System.out.println(list.get(2));
         System.out.println(list.size());
 
@@ -26,11 +25,44 @@ public class Hw13 {
         System.out.println(list);
 
 
-//        List<Integer> numbers = new ArrayList<>();
-//        for (int i = 100; i < 1001; i++) {
-//            numbers.add(i);
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 100; i < 1001; i++) {
+            numbers.add(i);
+        }
+
+        Iterator<Integer> iterator = numbers.iterator(); // создаем итератор
+        while (iterator.hasNext()) { //numbers.removeIf(item -> item % 2 == 0); лямбда метод
+            int item = iterator.next(); //следующий итератор
+            if (item % 2 == 0) { //
+                iterator.remove();
+            }
+        }
+        System.out.println(numbers);
+
+        //set() - заменяет элемент под итератором
+        //add() добавляет элемент после итератора
+
+//        for (Integer integer : numbers) {
+//            System.out.println(integer);
+//        }
+
+
+
+//        Iterator<Integer> iterator = numbers.iterator();
+//        Integer nextInt = iterator.next();
+//        // for (Integer integer : numbers) {
+//        if (nextInt % 2 == 0){
+//            nextInt.remove();
 //        }
 //
+//        while (iterator.hasNext()) {
+//            Integer nextNumber = iterator.next();
+//            if (nextNumber % 2 == 0) {
+//                numbers.remove(nextNumber);
+//            }
+//        }
+
+
 //        for (Integer i = 0; i < numbers.size(); i++) {
 //            if (numbers.get(i) % 2 == 0) {
 //                numbers.remove(i);
